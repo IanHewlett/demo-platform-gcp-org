5e38a72d608f4bdfa18047d738cb6498810ec0aa
Ian Hewlett <ian.w.hewlett@gmail.com>
2024-07-02 15:06:33 -0700

    remove now-unused shared app project iam module

--- modules/shared/gcp-iam-app/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "app_project_sa_email" {
-  value = google_service_account.app_service_account.email
-}
-
-output "app_project_sa_member" {
-  value = "serviceAccount:${google_service_account.app_service_account.email}"
-}
