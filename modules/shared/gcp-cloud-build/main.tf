005b31de1fa2aa0a5de767ebfcf123df83c28cf1
Ian Hewlett <ian.w.hewlett@gmail.com>
2024-07-03 21:43:58 -0700

    split cloud build module iam and service account into dedicated files

--- modules/shared/gcp-cloud-build/main.tf
+++ modules/shared/gcp-cloud-build/main.tf
@@ -1,22 +1,7 @@
-resource "google_service_account" "cloud_build_service_account" {
-  account_id  = "terraform-sa-build-submit"
-  description = "cloud build submitter service account"
-  project     = var.cicd_project_name
-}
-
 resource "google_storage_bucket" "cloud_build_bucket" {
   project                     = var.cicd_project_name
   name                        = "${var.cicd_project_name}-cloud-build"
   location                    = "US-CENTRAL1"
   force_destroy               = true
   uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket_iam_binding" "cloud_build_bucket_iam" {
-  bucket = google_storage_bucket.cloud_build_bucket.name
-  role   = "roles/storage.objectCreator"
-
-  members = [
-    "serviceAccount:${google_service_account.cloud_build_service_account.email}",
-  ]
 }
