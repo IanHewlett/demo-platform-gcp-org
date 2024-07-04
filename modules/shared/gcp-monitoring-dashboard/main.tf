2c3292f06ecef827ffe5ca49d277401ee30fa632
Ian Hewlett <ian.w.hewlett@gmail.com>
2024-07-03 21:18:35 -0700

    refactor dashboard json config into files dir

--- modules/shared/gcp-monitoring-dashboard/main.tf
+++ modules/shared/gcp-monitoring-dashboard/main.tf
@@ -1,7 +1,7 @@
 resource "google_monitoring_dashboard" "dashboard" {
   project = var.project_name
 
-  dashboard_json = templatefile("${path.module}/dashboard.json", {
+  dashboard_json = templatefile("${path.module}/files/dashboard.json", {
     environment = var.environment
   })
 }
